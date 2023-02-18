e05310f344f1514198c02d49a3a13c8a8bcbb60f
汤明 <infly123@sina.com>
2023-02-18 22:25:55 +0800

    config

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 )
 
 func main() {
+	log.Println("start")
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
