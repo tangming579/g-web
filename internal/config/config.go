956f3e13264023c071a87771749cd3fc99622469
汤明 <infly123@sina.com>
2023-02-21 22:27:07 +0800

    config

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,7 +7,7 @@ import (
 )
 
 var (
-	configPath = kingpin.Flag("config", "path to config file").Default(".").String()
+	configPath = *kingpin.Flag("config", "path to config file").Default(".").String()
 	AppConfig  *viper.Viper
 )
 
@@ -16,12 +16,17 @@ func init() {
 }
 
 func loadAppConfig() {
+	//初始化viper
 	AppConfig = viper.New()
+	//设置文件名
 	AppConfig.SetConfigName("app")
+	//设置文件类型
 	AppConfig.SetConfigType("yaml")
-	AppConfig.AddConfigPath(*configPath)
+	//设置文件所在路径
+	AppConfig.AddConfigPath(configPath)
 	err := AppConfig.ReadInConfig()
 	if err != nil {
+		//手动触发宕机，让程序崩溃
 		panic(fmt.Sprintf("读取配置文件失败:%s", err))
 	}
 	AppConfig.WatchConfig()
