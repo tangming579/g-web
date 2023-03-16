6f94e8c1375e28685952ad1ef514842d750d14e6
汤明 <infly123@sina.com>
2023-03-16 13:56:42 +0800

    consul

--- internal/http/server.go
+++ internal/http/server.go
@@ -2,6 +2,8 @@ package http
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/hashicorp/consul/api"
+	"go-web/internal/config"
 	"go-web/internal/http/middleware"
 	"go-web/internal/http/router"
 )
@@ -12,4 +14,17 @@ func NewServer(mode string) *gin.Engine {
 	middleware.LoadMiddleWare(server)
 	router.LoadRoutes(server)
 	return server
+}
+
+func NewConsulClient() (*api.Client, error) {
+	cCfg := &api.Config{
+		Address: config.AppConfig.Get("consul.address"),
+		Scheme:  config.AppConfig.Get("consul.scheme"),
+		Token:   config.AppConfig.Get("consul.token"),
+	}
+	clt, err := api.NewClient(cCfg)
+	if err != nil {
+		return nil, err
+	}
+	return clt, nil
 }
